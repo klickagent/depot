cf877dbc02d39816f792a5711d9d2365776d1859
klickagent <hello@klickagent.ch>
2013-03-05 14:43:48 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '4a664cb32c22daf727d728d714383c3d283cbd70fb2ad580d55b202ba28437df1e81f4c332b508e3eccb8915eadc73bff3ce0dec85953a04c2b0e6a20833c3d5'
