240367c85f66ae62c3109dbb755b24e5e9bc88f2
klickagent <hello@klickagent.ch>
2013-03-19 15:47:21 +0100

    new validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,22 +3,25 @@ class Product < ActiveRecord::Base
   validates_presence_of :title, :description
   validates :price, :numericality => {:greater_than_or_equal_to => 0.05}
   
+  #validates_format_of :title, :with => /.*title.*/i
+  
   validate :validate_title
   
   
 	# validates own business rules
 	protected
 	def validate_title
+	
 	  return unless self.no_tests_allowed
 	  errors.add(:fields, "title mustn't be test")
 	end
 	
 	protected
 	def no_tests_allowed
-		if self.title === "test"
-			return true
-		else
+		if !self.title.match(/.*test.*/i) && self.title.match(/.*title.*/i)
 			return false
+		else
+			return true
 		end
 	end
 
