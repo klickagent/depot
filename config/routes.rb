78a6780c9313100c4a9ef317b14b7d6433840d96
klickagent <hello@klickagent.ch>
2013-04-02 14:49:02 +0200

    functional tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Depot::Application.routes.draw do
   get "store/index"
 
-  resources :products
-
+  	resources :products
+  	resources :store
 	root to: 'store#index', as: 'store'
 
   # The priority is based upon order of creation:
